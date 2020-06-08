71e5541b18e6214da6d79e0ee1e45e8aeaaf12df
Victor Bolinches <vicboma1@users.noreply.github.com>
2020-06-09 00:02:02 +0200

    Update stdafx.h

--- iDmacDrv32/stdafx.h
+++ iDmacDrv32/stdafx.h
@@ -3,7 +3,8 @@
 DWORD __stdcall iDmacDrvOpen(int a1, LPVOID lp, LPVOID a3);
 DWORD __stdcall iDmacDrvClose(int a1, LPVOID lp);
 int   __stdcall iDmacDrvDmaRead(int a1, LPVOID lp, UINT_PTR ucb, LPVOID a4);
-int   __stdcall iDmacDrvDmaWrite(int a1, void *lp, UINT_PTR ucb, LPVOID a4);
+int   __stdcall iDmacDrvDmaWrite(int a1, LPVOID lp, UINT_PTR ucb, LPVOID a4);
 int   __stdcall iDmacDrvRegisterWrite(int a1, DWORD command, int data, LPVOID lp);
 int   __stdcall iDmacDrvRegisterRead(int a1, DWORD command, LPVOID ret_pointer, LPVOID a4);
-	  
+int   __stdcall iDmacDrvRegisterBufferRead(int a1, DWORD BytesReturned, LPVOID lp, UINT_PTR ucb, LPVOID a5);
+int   __stdcall iDmacDrvRegisterBufferWrite(int a1, DWORD BytesReturned, LPVOID lp, UINT_PTR ucb, LPVOID a5);
